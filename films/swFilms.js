0cf809bd1a214e808560cd4d647cb73d600c8986
JessicaBaros-mac <jessica@barosfamily.org>
2021-11-09 22:30:31 -0700

    Did I mean to leave this to the last minute? Nope.

--- films/swFilms.js
+++ films/swFilms.js
@@ -1,1 +1,20 @@
-import {films} from '../data/films';
+import {films} from '../data/films.js'
+
+let filmList = document.querySelector("#filmList")
+
+filmList.textContent = "This is my content... so weird right?"
+
+let myImg = document.createElement('img')
+myImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+
+filmList.appendChild(myImg);
+
+let titleList = document.createElement('ol')
+filmList.appendChild(titleList)
+
+for ( let i = 0; i < films.length; i++) {
+    
+    let listItem = document.createElement('li')
+    listItem.textContent = films[i].title
+    titleList.appendChild(listItem)
+}
