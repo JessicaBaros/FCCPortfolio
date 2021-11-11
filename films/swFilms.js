faac1893b9675c728cd75e8fd09214d17cdc6f5c
JessicaBaros-mac <jessica@barosfamily.org>
2021-11-10 17:49:01 -0700

    Basic Films list. Still need to match names

--- films/swFilms.js
+++ films/swFilms.js
@@ -1,20 +1,16 @@
-import {films} from '../data/films.js'
+import {films} from '../data/films.js';
 
-let filmList = document.querySelector("#filmList")
-
-filmList.textContent = "This is my content... so weird right?"
-
-let myImg = document.createElement('img')
-myImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
-
-filmList.appendChild(myImg);
-
-let titleList = document.createElement('ol')
-filmList.appendChild(titleList)
+let filmList = document.querySelector("#filmList");
 
 for ( let i = 0; i < films.length; i++) {
     
-    let listItem = document.createElement('li')
-    listItem.textContent = films[i].title
-    titleList.appendChild(listItem)
+    let figure = document.createElement('figure');
+    let figImg = document.createElement('img');
+            figImg.src = `https://starwars-visualguide.com/assets/img/films/${i+1}.jpg`;
+    let figCaption = document.createElement('figcaption');
+            figCaption.textContent = films[i].title;
+
+    figure.appendChild(figImg);
+    figure.appendChild(figCaption);
+    filmList.appendChild(figure);
 }
